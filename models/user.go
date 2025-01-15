2e650fe5c061885dbce5751ee87a625ff0a3e652
ariefcatur <muhammadariefcp@gmail.com>
2025-01-15 16:50:34 +0700

    add Email params

--- models/user.go
+++ models/user.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 	Username string `json:"username" gorm:"unique;not null"`
+	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `json:"-" gorm:"not null"`
 	Tasks    []Task `json:"tasks"`
 }
